99c8bd2c1057d412d8d1e6444504650732b061d4
Valentina Sulbaran <vjss05@gmail.com>
2022-01-25 13:02:27 +0100

    toggle function in progress

--- CODE_SAMPLE_BASIC/main.js
+++ CODE_SAMPLE_BASIC/main.js
@@ -8,18 +8,28 @@ let options = ['this','this not', 'this either']
 let states = [false,false,false]
 let correct_answer_index= 0
 
+document.addEventListener('DOMContentLoaded', question)
 document.addEventListener('DOMContentLoaded', init)
+
+function question(){
+	let questionDisplay = document.querySelector('#question-wrapper')
+	questionDisplay.innerHTML+= "<p> Parts of speech indicate when something happened.</p>"
+	
+	// ...
+}
 
 function init(){
 	let optionsContainer=document.querySelector('#options-wrapper')
 	for(let i = 0; i< options.length; i++){
-		optionsContainer.innerHTML+= "<div class='unchosen option'><p class='text'>"+options[i]+"</p></div>"
+		optionsContainer.innerHTML+= `<div class='unchosen option' onclick="toggleChoice(${i})"><p class='text'> ${options[i]} </p></div>`
 	}
-	// ...
+
 }
 
 function toggleChoice(i){
 	states[i]=true
+	console.log('hello')
+	let selectedOption = document.querySelector(".chosen").style.border = "4px solid rgb(88, 86, 86);"
 	// ...
 }
 
